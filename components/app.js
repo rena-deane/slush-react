f6bec8a4dd20c5fd466130f8b358fa4fd9542e71
Rena Goldsmith <rena.kg@gmail.com>
2016-07-05 11:46:59 +1200

    setup presets and render conuter

--- components/app.js
+++ components/app.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-class App extends Component {
+class Greeter extends Component {
 
   constructor (props) {
     super(props)
@@ -12,4 +12,40 @@ class App extends Component {
 
 }
 
+class App extends Component {
+    constructor (props) {
+        super(props)
+    }
+    render() {
+        return (
+            <div>
+                <Greeter name="Slushy"/>
+                <Counter />
+                <Counter />
+                <Counter />
+                <Counter />
+            </div>
+        )
+    }
+}
+
+class Counter extends Component {
+    constructor (props) {
+        super(props)
+        this.state = {counter: 0}
+    }
+    addOne(){
+        this.setState({counter: this.state.counter + 1})
+    }
+    render() {
+        return (
+            <div>
+                <h2>This is my counter: {this.state.counter}</h2>
+                <button onClick={this.addOne.bind(this)}>MORE</button>
+            </div>
+        )
+    }
+}
+
 export default App
+
--- index.js
+++ index.js
@@ -2,5 +2,5 @@ import React from 'react'
 import { render } from 'react-dom'
 import App from './components/app'
 
-render(<App name='Slushy-React' />, document.querySelector('main'))
+render(<App />, document.querySelector('main'))
 console.log('welcome to Slushy-React')
